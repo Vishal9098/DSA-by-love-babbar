facd25c44f55a972195ab6be36d12cf80e9d88ce
Vishal Parmar <94777911+Vishal9098@users.noreply.github.com>
2022-08-24 11:50:55 +0530

    Add files via upload

---
+++ DSA Code Help/C++/Patterns/pattern3.cpp
@@ -0,0 +1,30 @@
+/*same row same columns 
+    different number*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int i=1;
+
+    while (i<=n)
+    {
+        /* code */
+        int j=1;
+        while (j<=n)
+        {
+            /* code */
+            cout<<j;
+            j++;
+        }
+    cout<<endl;
+    i++;
+    }
+    return 0;
+}
